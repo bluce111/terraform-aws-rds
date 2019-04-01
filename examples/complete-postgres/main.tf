fee50f27206c2c5a6f5451f2836017db3f213e29
bluce111 <35945020+bluce111@users.noreply.github.com>
2019-04-01 16:26:21 -0400

    mybranchmod

--- examples/complete-postgres/main.tf
+++ examples/complete-postgres/main.tf
@@ -1,6 +1,13 @@
 provider "aws" {
-  region = "us-west-1"
+  region  = "${var.aws_region}"
+#  profile = "${var.aws_profile}"
 }
+
+#update region and profile (if needed)
+
+#to make publicly accessible, change variable in the db instance module
+#otherwise a bastion host will need to be used.
+
 
 ##############################################################
 # Data sources to get VPC, subnets and security group details
@@ -22,25 +29,25 @@ data "aws_security_group" "default" {
 # DB
 #####
 module "db" {
-  source = "../../"
+  source = "./terraform-aws-rds"
 
-  identifier = "demodb"
+  identifier = "twdpostgres"
 
   engine            = "postgres"
-  engine_version    = "9.6.3"
-  instance_class    = "db.t2.large"
+  engine_version    = "10.6"
+  instance_class    = "db.t2.medium"
   allocated_storage = 5
   storage_encrypted = false
 
-  # kms_key_id        = "arm:aws:kms:<region>:<account id>:key/<kms key id>"
-  name = "demodb"
+ #kms_key_id        = "arm:aws:kms:<region>:<account id>:key/<kms key id>"
+  name = "twdpostgres"
 
   # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
   # "Error creating DB Instance: InvalidParameterValue: MasterUsername
   # user cannot be used as it is a reserved word used by the engine"
-  username = "demouser"
+  username = "twdpostgres"
 
-  password = "YourPwdShouldBeLongAndSecure!"
+  password = "t3stdb12345"
   port     = "5432"
 
   vpc_security_group_ids = ["${data.aws_security_group.default.id}"]
@@ -52,7 +59,7 @@ module "db" {
   backup_retention_period = 0
 
   tags = {
-    Owner       = "user"
+    Owner       = "twdpostgres"
     Environment = "dev"
   }
 
@@ -62,14 +69,15 @@ module "db" {
   subnet_ids = ["${data.aws_subnet_ids.all.ids}"]
 
   # DB parameter group
-  family = "postgres9.6"
+  family = "postgres10"
 
   # DB option group
-  major_engine_version = "9.6"
+  major_engine_version = "10.6"
 
   # Snapshot name upon DB deletion
-  final_snapshot_identifier = "demodb"
+  final_snapshot_identifier = "twdpostgres"
 
   # Database Deletion Protection
-  deletion_protection = true
+  deletion_protection = false
 }
+
